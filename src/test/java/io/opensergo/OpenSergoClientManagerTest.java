18fd467fce46edb8965b141f79ce44afcf27e11b
lihuazeng <vartonli@chatly.com>
2023-03-11 16:53:07 +0800

    test2

--- src/test/java/io/opensergo/OpenSergoClientManagerTest.java
+++ src/test/java/io/opensergo/OpenSergoClientManagerTest.java
@@ -113,7 +113,7 @@ public class OpenSergoClientManagerTest {
     }
 
     @Test
-    public void testServerPushData() throws InterruptedException, InvalidProtocolBufferException {
+    public void testServerPushDataSubscriberSuccess() throws InterruptedException, InvalidProtocolBufferException {
         final AtomicReference<Object> actualData = new AtomicReference<>();
         final AtomicReference<SubscribeRequest> actualRequest = new AtomicReference<>();
         final AtomicReference<SubscribeResponse> expectedResponse = new AtomicReference<>();
@@ -137,42 +137,13 @@ public class OpenSergoClientManagerTest {
                         };
                     }
                 };
-        serviceRegistry.addService(serviceImpl);
 
-        // client call service
-        SubscribeKey subscribeKey = new SubscribeKey("default", "my-service2", ConfigKind.FAULT_TOLERANCE_RULE);
-        client.subscribeConfig(subscribeKey, (subscribeConfigKey, data) -> {
+        // client side subscriber
+        OpenSergoConfigSubscriber configSubscriber = (subscribeKey, data) -> {
             actualData.set(data);
             return true;
-        });
-
-        // wait for request finish
-        Thread.sleep(2000);
-        assertNotNull(actualRequest.get());
-        assertNotNull(actualRequest.get().getTarget());
-        assertEquals(subscribeKey.getApp(), actualRequest.get().getTarget().getApp());
-        assertEquals(subscribeKey.getNamespace(), actualRequest.get().getTarget().getNamespace());
-        assertEquals(subscribeKey.getKind().getKindName(), actualRequest.get().getTarget().getKinds(0));
-
-        assertNotNull(actualData.get());
-        assertTrue(actualData.get() instanceof List);
-        List<Object> dataList = (List<Object>) actualData.get();
-        assertNotNull(dataList.get(0));
-        assertEquals(1, dataList.size());
-        assertTrue(dataList.get(0) instanceof FaultToleranceRule);
-        FaultToleranceRule faultToleranceRule = (FaultToleranceRule) dataList.get(0);
-        assertNotNull(faultToleranceRule.getAction());
-
-        assertNotNull(expectedResponse.get());
-        assertNotNull(expectedResponse.get().getDataWithVersion());
-        Any expectedData = expectedResponse.get().getDataWithVersion().getData(0);
-        assertNotNull(expectedData);
-        assertTrue(expectedData.is(FaultToleranceRule.class));
-        FaultToleranceRule  expectedFaultToleranceRule = expectedData.unpack(FaultToleranceRule.class);
-
-        assertEquals(expectedFaultToleranceRule.getAction(), faultToleranceRule.getAction());
-        assertEquals(expectedFaultToleranceRule.getStrategiesList(), faultToleranceRule.getStrategiesList());
-        assertEquals(expectedFaultToleranceRule.getTargetsList(), faultToleranceRule.getTargetsList());
+        };
+        testServerPushFaultToleranceRule(configSubscriber, serviceImpl, actualData, actualRequest, expectedResponse);
     }
 
     @Test
@@ -199,10 +170,9 @@ public class OpenSergoClientManagerTest {
                             responseObserver.onNext(response);
 
                             try {
-                                // wait for first pushing finish
+                                // wait for first pushing client finish
                                 Thread.sleep(1000);
-                            } catch (InterruptedException ignored) {
-                            }
+                            } catch (InterruptedException ignored) {}
                             // push second data, but it is outdated.
                             DataWithVersion dataWithVersion = DataWithVersion.newBuilder().setVersion(0)
                                     .addData(response.getDataWithVersion().getDataList().get(0)).build();
@@ -218,42 +188,13 @@ public class OpenSergoClientManagerTest {
                         };
                     }
                 };
-        serviceRegistry.addService(serviceImpl);
 
-        // client call service
-        SubscribeKey subscribeKey = new SubscribeKey("default", "my-service2", ConfigKind.FAULT_TOLERANCE_RULE);
-        client.subscribeConfig(subscribeKey, (subscribeConfigKey, data) -> {
+        // client side subscriber
+        OpenSergoConfigSubscriber configSubscriber = (subscribeKey, data) -> {
             actualData.set(data);
             return true;
-        });
-
-        // wait for request/response finish
-        Thread.sleep(2000);
-        assertNotNull(actualRequest.get());
-        assertNotNull(actualRequest.get().getTarget());
-        assertEquals(subscribeKey.getApp(), actualRequest.get().getTarget().getApp());
-        assertEquals(subscribeKey.getNamespace(), actualRequest.get().getTarget().getNamespace());
-        assertEquals(subscribeKey.getKind().getKindName(), actualRequest.get().getTarget().getKinds(0));
-
-        assertNotNull(actualData.get());
-        assertTrue(actualData.get() instanceof List);
-        List<Object> dataList = (List<Object>) actualData.get();
-        assertNotNull(dataList.get(0));
-        assertEquals(1, dataList.size());
-        assertTrue(dataList.get(0) instanceof FaultToleranceRule);
-        FaultToleranceRule faultToleranceRule = (FaultToleranceRule) dataList.get(0);
-        assertNotNull(faultToleranceRule.getAction());
-
-        assertNotNull(expectedResponse.get());
-        assertNotNull(expectedResponse.get().getDataWithVersion());
-        Any expectedData = expectedResponse.get().getDataWithVersion().getData(0);
-        assertNotNull(expectedData);
-        assertTrue(expectedData.is(FaultToleranceRule.class));
-        FaultToleranceRule  expectedFaultToleranceRule = expectedData.unpack(FaultToleranceRule.class);
-
-        assertEquals(expectedFaultToleranceRule.getAction(), faultToleranceRule.getAction());
-        assertEquals(expectedFaultToleranceRule.getStrategiesList(), faultToleranceRule.getStrategiesList());
-        assertEquals(expectedFaultToleranceRule.getTargetsList(), faultToleranceRule.getTargetsList());
+        };
+        testServerPushFaultToleranceRule(configSubscriber, serviceImpl, actualData, actualRequest, expectedResponse);
     }
 
     @Test
@@ -288,74 +229,13 @@ public class OpenSergoClientManagerTest {
             // throw error
             throw new RuntimeException("SUBSCRIBE_HANDLER_ERROR");
         };
-        testServerPushData2(configSubscriber, serviceImpl, actualData, actualRequest, expectedResponse);
-
-//        final AtomicReference<Object> actualData = new AtomicReference<>();
-//        final AtomicReference<SubscribeRequest> actualRequest = new AtomicReference<>();
-//        final AtomicReference<SubscribeResponse> expectedResponse = new AtomicReference<>();
-//
-//        // implement the fake service
-//        OpenSergoUniversalTransportServiceGrpc.OpenSergoUniversalTransportServiceImplBase serviceImpl =
-//                new OpenSergoUniversalTransportServiceGrpc.OpenSergoUniversalTransportServiceImplBase(){
-//                    @Override
-//                    public StreamObserver<SubscribeRequest> subscribeConfig(StreamObserver<SubscribeResponse> responseObserver) {
-//                        return (FakerStreamObserver) subscribeRequest -> {
-//                            if (StringUtils.isBlank(subscribeRequest.getResponseAck())) {
-//                                actualRequest.set(subscribeRequest);
-//                            }else {
-//                                // from client ack, skip
-//                                return;
-//                            }
-//                            SubscribeResponse response = buildSpecificFaultToleranceRuleSubscribeResponse();
-//                            expectedResponse.set(response);
-//                            responseObserver.onNext(response);
-//                            responseObserver.onCompleted();
-//                        };
-//                    }
-//                };
-//        serviceRegistry.addService(serviceImpl);
-//
-//        // client call service
-//        SubscribeKey subscribeKey = new SubscribeKey("default", "my-service2", ConfigKind.FAULT_TOLERANCE_RULE);
-//        client.subscribeConfig(subscribeKey, (subscribeConfigKey, data) -> {
-//            actualData.set(data);
-//            // throw error
-//            throw new RuntimeException("SUBSCRIBE_HANDLER_ERROR");
-//        });
-//
-//        // wait for request/response finish
-//        Thread.sleep(2000);
-//        assertNotNull(actualRequest.get());
-//        assertNotNull(actualRequest.get().getTarget());
-//        assertEquals(subscribeKey.getApp(), actualRequest.get().getTarget().getApp());
-//        assertEquals(subscribeKey.getNamespace(), actualRequest.get().getTarget().getNamespace());
-//        assertEquals(subscribeKey.getKind().getKindName(), actualRequest.get().getTarget().getKinds(0));
-//
-//        assertNotNull(actualData.get());
-//        assertTrue(actualData.get() instanceof List);
-//        List<Object> dataList = (List<Object>) actualData.get();
-//        assertNotNull(dataList.get(0));
-//        assertEquals(1, dataList.size());
-//        assertTrue(dataList.get(0) instanceof FaultToleranceRule);
-//        FaultToleranceRule faultToleranceRule = (FaultToleranceRule) dataList.get(0);
-//        assertNotNull(faultToleranceRule.getAction());
-//
-//        assertNotNull(expectedResponse.get());
-//        assertNotNull(expectedResponse.get().getDataWithVersion());
-//        Any expectedData = expectedResponse.get().getDataWithVersion().getData(0);
-//        assertNotNull(expectedData);
-//        assertTrue(expectedData.is(FaultToleranceRule.class));
-//        FaultToleranceRule  expectedFaultToleranceRule = expectedData.unpack(FaultToleranceRule.class);
-//
-//        assertEquals(expectedFaultToleranceRule.getAction(), faultToleranceRule.getAction());
-//        assertEquals(expectedFaultToleranceRule.getStrategiesList(), faultToleranceRule.getStrategiesList());
-//        assertEquals(expectedFaultToleranceRule.getTargetsList(), faultToleranceRule.getTargetsList());
+        testServerPushFaultToleranceRule(configSubscriber, serviceImpl, actualData, actualRequest, expectedResponse);
     }
 
-    private void testServerPushData2(OpenSergoConfigSubscriber subscriber,
-                                     OpenSergoUniversalTransportServiceGrpc.OpenSergoUniversalTransportServiceImplBase serviceImpl,
-                                     AtomicReference<Object> actualData, AtomicReference<SubscribeRequest> actualRequest,
-                                     AtomicReference<SubscribeResponse> expectedResponse) throws InterruptedException, InvalidProtocolBufferException {
+    private void testServerPushFaultToleranceRule(OpenSergoConfigSubscriber subscriber,
+                                                  OpenSergoUniversalTransportServiceGrpc.OpenSergoUniversalTransportServiceImplBase serviceImpl,
+                                                  AtomicReference<Object> actualData, AtomicReference<SubscribeRequest> actualRequest,
+                                                  AtomicReference<SubscribeResponse> expectedResponse) throws InterruptedException, InvalidProtocolBufferException {
 
         // registry fake service
         serviceRegistry.addService(serviceImpl);
